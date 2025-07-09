a5fe20933dc308b0ae1c79bf025334cddad5a9a5
jonysins2211 <Jonysins11220@gmail.com>
2025-07-10 02:47:51 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -9,8 +9,8 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHANNEL_ID = os.getenv("-1001948532295")
 
 # AMAZON
-AMAZON_ACCESS_KEY = os.getenv("AMAZON_ACCESS_KEY")
-AMAZON_SECRET_KEY = os.getenv("AMAZON_SECRET_KEY")
+AMAZON_ACCESS_KEY = os.getenv("AKPA645Y391752095807")
+AMAZON_SECRET_KEY = os.getenv("FDeCqkr0YyaX/X+Cdgwfpw6RfTjIq/30UB0LcV3h")
 PARTNER_TAG = os.getenv("ironman1122-21")
 AMAZON_HOST = os.getenv("webservices.amazon.in")
 AMAZON_REGION = os.getenv("eu-west-1")
