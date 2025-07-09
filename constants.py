9507d5875fea94532f8f28987707820ace9d9e61
jonysins2211 <Jonysins11220@gmail.com>
2025-07-10 02:37:28 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,11 +6,11 @@ load_dotenv()  # load variables from .env
 
 # TELEGRAM
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_ID = os.getenv("-1001948532295")
 
 # AMAZON
 AMAZON_ACCESS_KEY = os.getenv("AMAZON_ACCESS_KEY")
 AMAZON_SECRET_KEY = os.getenv("AMAZON_SECRET_KEY")
-PARTNER_TAG = os.getenv("PARTNER_TAG")
-AMAZON_HOST = os.getenv("AMAZON_HOST")
-AMAZON_REGION = os.getenv("AMAZON_REGION")
+PARTNER_TAG = os.getenv("ironman1122-21")
+AMAZON_HOST = os.getenv("webservices.amazon.in")
+AMAZON_REGION = os.getenv("eu-west-1")
